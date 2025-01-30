d3daa0096f91d4a2ab2d778f2a373d98fe2fa826
Andrii Huts <andriihuts05@gmail.com>
2025-01-30 11:45:16 +0300

    updatd modal position

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
-    <div class="container">
+    <div class="container flex flex-col justify-center items-center">
         @yield('content')
     </div>
 </body>
